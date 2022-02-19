2e20bc346f3f773edfaaa195195aae267acd0144
Otterpop7 <Jordanotterson7@gmail.com>
2022-02-19 08:54:37 -0700

    code snapshot
    starting point next lesson

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ var createTask = function(taskText, taskDate, taskList) {
     // append span and p element to parent li
     taskLi.append(taskSpan, taskP);
 
-    //check due date
+    // check due date
     auditTask(taskLi);
 
     // append to ul list on the page
@@ -35,7 +35,6 @@ var loadTasks = function() {
 
     // loop over object properties
     $.each(tasks, function(list, arr) {
-        console.log(list, arr);
         // then loop over sub-array
         arr.forEach(function(task) {
             createTask(task.text, task.date, list);
@@ -45,6 +44,32 @@ var loadTasks = function() {
 
 var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var auditTask = function(taskEl) {
+
+    // get date from task element
+    var date = $(taskEl)
+        .find("span")
+        .text()
+        .trim();
+
+    console.log(date);
+
+    // convert to moment object at 5:00pm
+    var time = moment(date, "L").set("hour", 17);
+
+    console.log(time);
+
+    // remove any old classes from element
+    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+    // apply new class if task is near/over due date
+    if (moment().isAfter(time)) {
+        $(taskEl).addClass("list-group-item-danger");
+    } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+        $(taskEl).addClass("list-group-item-warning");
+    }
 };
 
 // enable draggable/sortable feature on list-group elements
@@ -107,7 +132,6 @@ $("#trash").droppable({
     drop: function(event, ui) {
         // remove dragged element from the dom
         ui.draggable.remove();
-
     },
     over: function(event, ui) {
         console.log(ui);
@@ -117,6 +141,11 @@ $("#trash").droppable({
     }
 });
 
+// convert text field into a jquery date picker
+$("#modalDueDate").datepicker({
+    // force user to select a future date
+    minDate: 1
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -194,10 +223,6 @@ $(".list-group").on("blur", "textarea", function() {
     $(this).replaceWith(taskP);
 });
 
-$("#modalDueDate").datepicker({
-    minDate: 1
-});
-
 // due date was clicked
 $(".list-group").on("click", "span", function() {
     // get current text
@@ -212,11 +237,11 @@ $(".list-group").on("click", "span", function() {
         .val(date);
     $(this).replaceWith(dateInput);
 
-    // enable jquery ui datepicker
+    // enable jquery ui date picker
     dateInput.datepicker({
         minDate: 1,
         onClose: function() {
-            //when calender is closed, force a "change" event on the `dateInput`
+            // when calendar is closed, force a "change" event
             $(this).trigger("change");
         }
     });
@@ -224,24 +249,6 @@ $(".list-group").on("click", "span", function() {
     // automatically bring up the calendar
     dateInput.trigger("focus");
 });
-
-var auditTask = function(taskEl) {
-    // get date from task element
-    var date = $(taskEl).find("span").text().trim();
-
-    // convert to moment object at 5:00pm
-    var time = moment(date, "L").set("hour", 17);
-
-    // remove any old classes from element
-    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
-
-    // apply new class if task is near/over due date
-    if (moment().isAfter(time)) {
-        $(taskEl).addClass("list-group-item-danger");
-    } else if (Math.abs(moment().diff(time, "days")) <= 2) {
-        $(taskEl).addClass("list-group-item-warning");
-    }
-};
 
 // value of due date was changed
 $(".list-group").on("change", "input[type='text']", function() {
@@ -265,8 +272,6 @@ $(".list-group").on("change", "input[type='text']", function() {
         .addClass("badge badge-primary badge-pill")
         .text(date);
     $(this).replaceWith(taskSpan);
-
-    // Pass task's <li> element into auditTask() to check new due date
     auditTask($(taskSpan).closest(".list-group-item"));
 });
 
